2302fb24d944e55695b498b171a8632978aea31f
andrei marotta <andreiv.marotta@gmail.com>
2024-07-18 07:22:38 +0000

    class 6

--- Class6-workshop/main.tf
+++ Class6-workshop/main.tf
@@ -3,6 +3,10 @@ provider aws {
 }
 
 resource "aws_key_pair" "deployer" {
-    key_name = 
+    key_name = "hello-key"
     public_key = file("~/.ssh/id_rsa.pub")
+}
+
+variable region {
+  type        = string
 }
